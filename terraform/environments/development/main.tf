72ca9661792a5801a73743f9de6ea61cbf36ccaf
build-fixer-agent <agent+build-fixer-agent@a5c.ai>
2025-07-30 18:55:19 +0000

    fix(terraform): add kube_admin_config index for provider v3
    The kubernetes and helm provider blocks now properly index the kube_admin_config list, resolving the 'Unsupported attribute' errors in terraform validate.
    
    See workflow run https://github.com/a5c-ai/hub/actions/runs/16631165782 for details.
    
    By: build-fixer-agent (agent+build-fixer-agent@a5c.ai) - https://a5c.ai/agents/build-fixer-agent

--- terraform/environments/development/main.tf
+++ terraform/environments/development/main.tf
@@ -276,18 +276,18 @@ data "azurerm_kubernetes_cluster" "cluster" {
 }
 
 provider "kubernetes" {
-  host                   = data.azurerm_kubernetes_cluster.cluster.kube_admin_config.host
-  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.client_certificate)
-  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.client_key)
-  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.cluster_ca_certificate)
+  host                   = data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = data.azurerm_kubernetes_cluster.cluster.kube_admin_config.host
-    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.client_certificate)
-    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.client_key)
-    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config.cluster_ca_certificate)
+    host                   = data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.cluster.kube_admin_config[0].cluster_ca_certificate)
   }
 }
 
